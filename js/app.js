26f8ed0be875cbaad6b0033c35121e38a55982c4
krrsalmane <karroumsalmane@gmail.com>
2025-11-05 14:42:59 +0100

    add static data

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+const CARD_DATA = [
+    { id: 1, name: "Aetherius, Sky Weaver", rarity: "Legendary", price: 99.99, image: "images/sky.png", description: "A being of pure starlight, able to bend constellations to its will." },
+    { id: 2, name: "Gravemaw the Devourer", rarity: "Mythic", price: 149.99, image: "images/graveman.png", description: "It is said that entire worlds have vanished into its endless maw." },
+    { id: 3, name: "Celestial Guardian", rarity: "Epic", price: 49.99, image: "images/angel.png", description: "An eternal sentinel sworn to protect the celestial gates." },
+    { id: 4, name: "Shadowfen Ambusher", rarity: "Rare", price: 19.99, image: "images/shadowman.png", description: "Strikes from the shadows with poison-tipped blades." },
+    { id: 5, name: "Sunstone Paladin", rarity: "Epic", price: 55.00, image: "images/hammer.png", description: "Wields the power of the sun to smite his foes." },
+    { id: 6, name: "Riftwalker Mage", rarity: "Legendary", price: 89.99, image: "images/rifwalker.png", description: "Steps between dimensions as easily as one might walk across a room." },
+    { id: 7, name: "Ironclad Golem", rarity: "Common", price: 5.99, image: "images/ironclad.png", description: "Animated by simple enchantment, it is a stalwart defender." },
+    { id: 8, name: "Forest Whisperer", rarity: "Rare", price: 22.50, image: "images/forestwhisper.png", description: "Can hear the ancient secrets carried on the wind." },
+    { id: 9, name: "Goblin Spark-Flinger", rarity: "Common", price: 4.50, image: "images/goblin.png", description: "Loves anything that goes 'boom'. Usually from a safe distance." }
+];
